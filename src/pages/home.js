afa1ee8451375c131d3db0b5938eba41af766208
System Administrator <root@MacBook-Pro-de-Mateo.local>
2023-03-29 09:32:21 -0500

    build definitivo

---
+++ src/pages/home.js
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+import "../assets/css/DiscountCalculator.css";
+import swal from "sweetalert";
+
+function App() {
+  const [price, setPrice] = useState("");
+  const [discount, setDiscount] = useState("");
+
+  const calculateDiscount = () => {
+    if (price <= 0 || discount <= 0 || discount >= 100) {
+      swal({
+        title: "Error!",
+        text: "Ingrese valores válidos",
+        icon: "error",
+        button: "OK",
+      });
+      return;
+    }
+
+    const discountedPrice = price - (price * discount) / 100;
+
+    swal({
+      title: "Precio con descuento:",
+      text: `$${discountedPrice.toFixed(2)}`,
+      icon: "success",
+      button: "OK",
+    }).then(() => {
+      setPrice("");
+      setDiscount("");
+    });
+  };
+
+  return (
+    <div className="App">
+      <h1>Calculadora de Descuentos</h1>
+      <input
+      className="input"
+        type="number"
+        placeholder="Precio"
+        value={price}
+        onChange={(e) => setPrice(e.target.value)}
+      />
+      <br />
+      <input
+      className="input"
+        type="number"
+        placeholder="% de descuento"
+        value={discount}
+        onChange={(e) => setDiscount(e.target.value)}
+      />
+      <br />
+      <button className="calculate-btn" onClick={calculateDiscount}>Calcular</button>
+    </div>
+  );
+}
+
+export default App;
